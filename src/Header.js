a1c7f0ec6b4f2d4deffadadda622376ba39998b3
prakash-2021 <prakashrestha2075@gmail.com>
2022-07-25 15:16:40 +0545

    imported

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export function Header() {
+    return (
+      <header>
+        <nav className="nav">
+          <img src="./logo192.png" className="nav-logo" />
+          <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Header } from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const navbar = (
+//   <nav>
+//       <h1>Bob's Bistro</h1>
+//       <ul>
+//           <li>Menu</li>
+//           <li>About</li>
+//           <li>Contact</li>
+//       </ul>
+//   </nav>
+// )
+
+//simple static page challange
+
+// const page = (
+//   <>
+//   <img src="./logo192.png" />
+//   <h1>Fun facts about react</h1>
+//   <ul>
+//     <li>Was first released in 2013</li>
+//     <li>Was orirginally created byy Jordan Walke</li>
+//     <li>Has well over 100k stars on Github</li>
+//     <li>Is maintained by Facebook</li>
+//     <li>Powers thousands of enterprise apps, including mobile apps</li>
+//   </ul>
+//   </>
+// )
+
+//custom component
+
+
+const app = ReactDOM.createRoot(document.getElementById("root"));
+
+app.render(
+  <>
+    <Header />
+    <MainContent />
+    <Footer />
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
